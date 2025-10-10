d175cf0ac835e22e282c7507d8ff35d14ec89d92
LucasGoulart24 <Goulart7720@gmail.com>
2025-10-10 14:05:26 -0300

    musica

---
+++ script.js
@@ -0,0 +1,23 @@
+let capaMusic = document.getElementById('capa');
+let tituloMusic = document.getElementById('titulo');
+let autorMusic = document.getElementById('banda');
+
+const Musics = [
+    {
+        capaMusic: './Img/Capa_da_musica.jpeg',
+        tituloMusic: 'Mary on Cross',
+        autorMusic: 'Ghost',
+        Audio: './Music/Ghost - Mary On A Cross (Official Audio) - Ghost (youtube).mp3'
+    },
+    {
+        capaMusic: './Img/Capa_da_musica2.jpeg',
+        tituloMusic: 'Past Lives',
+        autorMusic: 'Sapientdream',
+        Audio: './Music/sapientdream, Slushii - Past Lives (Official Lyric Video) - sapientdream (youtube).mp3'
+    }
+];
+
+// Função para tocar a musica
+function PlayerMusic() {
+    addEventListener('click', function() {})
+}
